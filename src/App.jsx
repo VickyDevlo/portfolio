9d98d32978878d9ea027c5f9ceef115eccb4ea88
vickyDevlo <vickymagare1996@gmail.com>
2024-11-26 13:44:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-center py-12 text-3xl font-semibold">
+        Portfolio Project
+      </h1>
+    </>
+  );
+}
+
+export default App;
