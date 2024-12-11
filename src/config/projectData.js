e5e46e711ed4652a96c5792f57c1741a70931581
vickyDevlo <vickymagare1996@gmail.com>
2024-12-11 18:54:42 +0530

    project added

--- src/config/projectData.js
+++ src/config/projectData.js
@@ -1,30 +1,31 @@
-import movixPic from '../assets/SharedScreenshot_movies.jpg'
-import irrigationPic from '../assets/SharedScreenshot_saiMalhar.jpg'
-import  smrPic from '../assets/SharedScreenshot_smr.jpg'
+import movixPic from "../assets/SharedScreenshot_movies.jpg"; 
+import imagifyPic from "../assets/SharedScreenshot_imagify.jpg"; 
+import smrPic from "../assets/SharedScreenshot_smr.jpg";
 
 export const projectData = [
   {
     id: 1,
-    title: 'Movix: Movies & Tv Shows App',
-    description: 'Explore the latest movies, binge-worthy TV shows, and detailed info about your favorite titles—all in one place.',
-    image: movixPic,
-    demoLink: 'https://movies-five-wheat.vercel.app', 
+    title: "Imagify",
+    description:
+      "Turn your imagination into visual art in seconds - just type, and watch the magic happen",
+    image: imagifyPic,
+    demoLink: "https://sass-image-generator.vercel.app",
   },
   {
     id: 2,
-    title: 'SMR Enterprise Quality Glass Solutions',
-    description: 'Premium glass doors and windows designed for modern living.',
-    image: smrPic,
-    demoLink: 'https://smr-enterprise.vercel.app', 
+    title: "Movix: Movies & Tv Shows App",
+    description:
+      "Explore the latest movies, binge-worthy TV shows, and detailed info about your favorite titles—all in one place.",
+    image: movixPic,
+    demoLink: "https://movies-five-wheat.vercel.app",
   },
-  // {
-  //   id: 2,
-  //   title: 'Irrigation Equipment',
-  //   description: 'Explore a comprehensive catalog of irrigation equipment, including drip systems, sprinklers, and accessories. Find the perfect solution for your farming needs.',
-  //   image: irrigationPic,
-  //   demoLink: 'https://drip-irrigation-ashen.vercel.app/', 
-  // },
-   
+  {
+    id: 3,
+    title: "SMR Enterprise Quality Glass Solutions",
+    description: "Premium glass doors and windows designed for modern living.",
+    image: smrPic,
+    demoLink: "https://smr-enterprise.vercel.app",
+  },
 ];
 
 export default projectData;
