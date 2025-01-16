f96ad45b75ed530c0c9363f8a5e996005405363a
vickyDevlo <vickymagare1996@gmail.com>
2025-01-16 15:17:02 +0530

    changes done

--- src/config/projectData.js
+++ src/config/projectData.js
@@ -1,10 +1,19 @@
 import movixPic from "../assets/SharedScreenshot_movies.jpg";
 import imagifyPic from "../assets/SharedScreenshot_imagify.jpg";
 import smrPic from "../assets/SharedScreenshot_smr.jpg";
+import JobPortalPic from "../assets/SharedScreenshot_jobPortal.jpg";
 
 export const projectData = [
   {
     id: 1,
+    title: "Explore Top IT Jobs | Job Portal",
+    description:
+      "Find the best IT job opportunities in software development, cybersecurity, data science, and more. Connect with leading tech companies and build your dream. Start applying today!",
+    image: JobPortalPic,
+    demoLink: "https://job-portal-new-mauve.vercel.app",
+  },
+  {
+    id: 2,
     title: "Imagify",
     description:
       "Turn your imagination into visual art in seconds - just type, and watch the magic happen",
@@ -12,7 +21,7 @@ export const projectData = [
     demoLink: "https://image-generator-blond.vercel.app",
   },
   {
-    id: 2,
+    id: 3,
     title: "Movix: Movies & Tv Shows App",
     description:
       "Explore the latest movies, binge-worthy TV shows, and detailed info about your favorite titles—all in one place.",
@@ -20,7 +29,7 @@ export const projectData = [
     demoLink: "https://movies-five-wheat.vercel.app",
   },
   {
-    id: 3,
+    id: 4,
     title: "SMR Enterprise Quality Glass Solutions",
     description: "Premium glass doors and windows designed for modern living.",
     image: smrPic,
