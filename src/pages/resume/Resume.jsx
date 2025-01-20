d5e094276cb7e9ddb15c3689f0da847f3ee973c5
vickyDevlo <vickymagare1996@gmail.com>
2025-01-20 17:41:49 +0530

    changes done

--- src/pages/resume/Resume.jsx
+++ src/pages/resume/Resume.jsx
@@ -5,17 +5,32 @@ import { FaFileDownload } from "react-icons/fa";
 import resume_pic from "../../assets/resume_Pic.svg";
 
 const Resume = () => {
+  const handleDownload = () => {
+    if (!pranav_resume) {
+      console.error("Resume file is missing!");
+      return;
+    }
+
+    const link = document.createElement("a");
+    link.href = pranav_resume;
+    link.download = "pranav_resume.pdf";
+
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <Wrapper>
       <div className="w-full flex justify-evenly items-center md:items-start flex-col md:flex-row gap-8 md:gap-12 px-5 py-10">
-        <a
-          href={pranav_resume}
-          download="pranav_resume.pdf"
-          className="flex items-center gap-3 text-xl md:text-3xl px-4 py-2 bg-[#3a1d67] text-white rounded-lg shadow-md hover:bg-[#3f2a5e] transition duration-300 ease-in-out"
+        <button
+          onClick={handleDownload}
+          className="flex items-center gap-3 text-xl md:text-3xl px-4 py-2 bg-[#3a1d67] text-white rounded-lg shadow-md hover:bg-[#3f2a5e] 
+          transition duration-300 ease-in-out"
         >
           <FaFileDownload className="text-lg" size={30} />
           Download CV
-        </a>
+        </button>
         <div className="mt-3 w-full max-w-[400px] md:max-w-[500px] md:h-[500px]">
           <img
             src={resume_pic}
