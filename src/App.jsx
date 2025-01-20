bfd60478379a7d4f07c69615b6b2402755b06b1b
vickyDevlo <vickymagare1996@gmail.com>
2025-01-20 18:00:44 +0530

    changes done

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,10 @@ import { Router } from "./components/Router";
 import { useTheme } from "./context/ThemeContext";
 import { useScrollToTop } from "./hooks/useScrollUp";
 import PageUp from "./shared/pageUp/PageUp";
-import pranav from "../public/resume.pdf"
+import {resume as pranav_resume} from "./assets/resume.pdf";
 
 function App() {
-  console.log("pranav", pranav);
-
+  console.log("pranav", pranav_resume);
   const { isDarkMode } = useTheme();
   useScrollToTop();
 
