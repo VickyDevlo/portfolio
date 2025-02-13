c3c27adccab20d2b8641020f4b6e4fb7cd5de494
vickyDevlo <vickymagare1996@gmail.com>
2025-02-13 11:19:36 +0530

    changes done

--- src/config/projectData.js
+++ src/config/projectData.js
@@ -1,12 +1,20 @@
 import movixPic from "../assets/SharedScreenshot_movies.png";
 import imagifyPic from "../assets/SharedScreenshot_imagify.png";
-import smrPic from "../assets/SharedScreenshot_smr.png";
+import InterviewSchedulePic from "../assets/SharedScreenshot_interview-scheduler.jpg";
 import JobPortalPic from "../assets/SharedScreenshot_jobPortal.png";
 import OtpVerification from "../assets/SharedScreenshototpGenerator.jpg";
 
 export const projectData = [
   {
     id: 1,
+    title: "Schedule Interview",
+    description:
+      " Candidate & Interviewer Management - Easily add, update, and remove candidates and interviewers.",
+    image: InterviewSchedulePic,
+    demoLink: "https://interview-scheduler-indol.vercel.app/dashboard",
+  },
+  {
+    id: 2,
     title: "Explore Top IT Jobs",
     description:
       "Find the best IT job opportunities in software development, cybersecurity, data science, and more. Connect with leading tech companies and build your dream. Start applying today!",
@@ -14,7 +22,7 @@ export const projectData = [
     demoLink: "https://job-portal-new-mauve.vercel.app",
   },
   {
-    id: 2,
+    id: 3,
     title: "OTP Verification",
     description:
       "Enhance your application's security with reliable and easy-to-integrate OTP (One-Time Password) verification",
@@ -22,7 +30,7 @@ export const projectData = [
     demoLink: "https://otp-generator-sigma.vercel.app",
   },
   {
-    id: 3,
+    id: 4,
     title: "Imagify",
     description:
       "Turn your imagination into visual art in seconds - just type, and watch the magic happen",
@@ -30,20 +38,13 @@ export const projectData = [
     demoLink: "https://image-generator-blond.vercel.app",
   },
   {
-    id: 4,
+    id: 5,
     title: "Movix: Movies & Tv Shows App",
     description:
       "Explore the latest movies, binge-worthy TV shows, and detailed info about your favorite titles—all in one place.",
     image: movixPic,
     demoLink: "https://movies-five-wheat.vercel.app",
   },
-  // {
-  //   id: 5,
-  //   title: "SMR Enterprise Quality Glass Solutions",
-  //   description: "Premium glass doors and windows designed for modern living.",
-  //   image: smrPic,
-  //   demoLink: "https://smr-enterprise.vercel.app",
-  // },
 ];
 
 export default projectData;
