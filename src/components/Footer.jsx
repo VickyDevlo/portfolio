ee4fa80bdcfa69ba6292c467a0083dc4f9d56608
vickyDevlo <vickymagare1996@gmail.com>
2026-08-03 12:57:12 +0530

    Changes done in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,14 +2,14 @@ export const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer
-      className="py-8 flex items-center justify-between border-t
-     border-mist/15"
-    >
-      <span className="font-mono text-xs text-mistdim">© {currentYear}</span>
-      <span className="font-mono text-xs text-ember">
-        Developed by Pranav
-      </span>
+    <footer className="mx-auto max-w-8xl px-6">
+      <div className="border-t border-mist/15 py-6" role="contentinfo">
+        <div className="text-center sm:flex-row">
+          <p className="text-center sm:text-left font-mono text-xs text-mistdim">
+            © {currentYear} Pranav Magare. All rights reserved.
+          </p>
+        </div>
+      </div>
     </footer>
   );
 };
