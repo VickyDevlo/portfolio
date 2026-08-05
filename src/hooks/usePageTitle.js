cc29c20aa70fca38fb665dfc7ea29f1cae4b8599
vickyDevlo <vickymagare1996@gmail.com>
2026-08-05 13:25:37 +0530

    site meta-data change

--- src/hooks/usePageTitle.js
+++ src/hooks/usePageTitle.js
@@ -1,7 +1,11 @@
 import { useEffect } from "react";
 
+const SITE_NAME = "Pranav.dev";
+
 export const usePageTitle = (title) => {
   useEffect(() => {
-    document.title = title ? `${title} - Portfolio` : "Portfolio";
+    document.title = title
+      ? `${title} | ${SITE_NAME}`
+      : `${SITE_NAME} | React & SAP UI5 Developer`;
   }, [title]);
 };
